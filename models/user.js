5fff5b837e301d8c65f354e8296b5fef11c63d20
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-04-13 13:03:35 +0500

    Создал схемы пользователя и карточки

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  about: {
+    type: String, // информация о себе — это строка
+    required: true, // оно должно быть у каждого пользователя, так что о себе — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  avatar: {
+    type: String, // информация о себе — это строка
+    required: true,
+  },
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('user', userSchema);
