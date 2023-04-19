ac433e036c9c05367918f430e1a3d843267a78b1
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-04-19 20:26:43 +0500

    Добавил комментарий

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const router = require('./routes/index');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
-const app = express();
+const app = express(); // Cоздаём приложение методом express
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
