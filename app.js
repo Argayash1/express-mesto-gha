9ecb834fdc332e3fd464559c9a8e6a1ce4e1a466
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-04-30 14:58:19 +0500

    Установил express-rate-limit и helmet

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -14,6 +23,10 @@ const app = express(); // Cоздаём приложение методом expr
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.use(helmet());
 
 app.use(express.json()); // для собирания JSON-формата
 app.use(express.urlencoded({ extended: true }));
